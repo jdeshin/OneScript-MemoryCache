4baea8074d9da028a9539f3ca3c8acffccdb3571
yury deshin <j.deshin@hotmail.com>
2018-03-20 16:23:42 +0300

    Add project files.

---
+++ OneScript-MemoryCache/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneScript_MemoryCache
+{
+    public class Class1
+    {
+    }
+}
